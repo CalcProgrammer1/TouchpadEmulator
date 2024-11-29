4a8ef05bb8b3733f544e5a9250d894a5112e7fed
Adam Honse <calcprogrammer1@gmail.com>
2024-11-29 01:58:31 -0600

    Kill existing instances of TouchpadEmulator before starting it from the LaunchTouchpadEmulator script

--- LaunchTouchpadEmulator.sh
+++ LaunchTouchpadEmulator.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
+# Kill all existing instances of TouchpadEmulator before starting a new one
+killall TouchpadEmulator
+
+# Load the uinput module and enable user access to input devices and uinput
+# This requires root permission, so use pkexec to show a graphical root prompt
 pkexec bash -c "chmod 666 /dev/input/*; modprobe uinput; chmod 777 /dev/uinput"
 
+# Start TouchpadEmulator
 TouchpadEmulator
